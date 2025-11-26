ef4de38c47b203a5540e35de15b906b7bac73d8e
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-11-26 17:39:12 +0100

    App - VoteOptions  files commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,35 @@
-import css from './App.module.css';
-import CafeInfo from '../CafeInfo/CafeInfo';
+import css from "./App.module.css";
+import { useState } from "react";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import type { Votes, VoteType } from "../../types/votes";
+
 
 export default function App() {
+    const [votes, setVotes] = useState<Votes>({
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    });
+
+    const handleVote = (type: VoteType): void => {
+        setVotes({
+            ...votes,
+            [type]: votes[type] + 1,
+        });
+    };
+    const resetVotes = (): void => {
+        setVotes({ good: 0, neutral: 0, bad: 0 });
+    };
+    
     return (
-        <>
-            <div className={css.app}>
-                <CafeInfo />
-            </div>
-        </>
+        <div className={css.app}>
+            <CafeInfo />
+            <VoteOptions
+                onVote={handleVote}
+                onReset={resetVotes}
+                canReset={true}
+            />
+        </div>
     );
 }
